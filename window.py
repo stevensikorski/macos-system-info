7f1a600170cd4702b20b45ff6b4f14cad4af0cba
stevensikorski <ssikorski03@gmail.com>
2022-08-26 00:38:34 -0400

    Attempted fix for dock name for windows.py

--- window.py
+++ window.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 from tkinter import * 
-import sys
 from sys import platform
 from Foundation import NSBundle
+import sys
 
 class Window:
     def __init__(self, window_width, window_height):
@@ -21,7 +23,7 @@ class Window:
         self.root.call('wm', 'iconphoto', self.root._w, PhotoImage(file=f'{sys.path[0]}/images/icon.png'))
         self.root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-    def mainLoop(self):
+    def startWindow(self):
         self.root.mainloop()
 
     def createLabel(self, text, column, row, padx, pady):
@@ -31,4 +33,4 @@ class Window:
 Container = Window(800, 500)
 Label1 = Container.createLabel("text1", 0, 0, 5, 5)
 Label2 = Container.createLabel("text2", 0, 1, 5, 5)
-Container.mainLoop()
+Container.startWindow()
